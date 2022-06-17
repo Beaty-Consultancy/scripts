bd996b70a8af242df7a98c66b22f16d16935ae99
beatyconsultancy <ricky@beatyconsultancy.co.uk>
2022-06-17 12:38:20 +0100

    Added new user creation scripts

---
+++ redhat/users/richmccaffery.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# File Name:    richmccaffery.sh
+# Description:  Add a richmccaffery user with his public keyu for Amazon Linux 2 boxes
+# Version:      1
+# Author:       richmccaffery
+# Date:         17/07/2022
+
+#######################################
+
+# Add richmccaffery's public key
+adduser richmccaffery
+usermod -aG wheel richmccaffery
+su -c 'mkdir /home/richmccaffery/.ssh' richmccaffery 
+su -c 'chmod 700 /home/richmccaffery/.ssh' richmccaffery
+su -c 'touch /home/richmccaffery/.ssh/authorized_keys' richmccaffery
+curl https://github.com/richmccaffery.keys >> /home/richmccaffery/.ssh/authorized_keys
+chmod 600 /home/richmccaffery/.ssh/authorized_keys
+echo "richmccaffery ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/90-cloud-init-users
